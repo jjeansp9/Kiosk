a12df17a4d62e4b47b6e8dd1751ddb663cee9bb8
박진솔 <jjeansp9@gmail.com>
2023-03-14 17:47:26 +0900

    commit

--- app/src/main/java/kr/co/kiosk/activities/SettingMenuActivity.java
+++ app/src/main/java/kr/co/kiosk/activities/SettingMenuActivity.java
@@ -2,15 +2,30 @@ package kr.co.kiosk.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import kr.co.kiosk.R;
+import kr.co.kiosk.databinding.ActivitySettingMenuBinding;
 
 public class SettingMenuActivity extends AppCompatActivity {
+
+    ActivitySettingMenuBinding binding;
+    int select= 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting_menu);
+        binding= ActivitySettingMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.coffee.setOnClickListener(v-> clickedCoffee());
+
+    }
+
+    void clickedCoffee(){
+        if (select % 2==0){binding.coffee.setBackgroundColor(Color.parseColor("#000000"));
+        }else{ binding.coffee.setBackgroundColor(Color.parseColor("#8A8A8A")); }
+        select++;
     }
 }
