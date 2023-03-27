9fcf155914c3f81ffcc71ba338787719481f2a0d
박진솔 <jjeansp9@gmail.com>
2023-03-27 14:14:38 +0900

    commit

--- app/src/main/java/kr/co/kiosk/activities/MainActivity.java
+++ app/src/main/java/kr/co/kiosk/activities/MainActivity.java
@@ -7,10 +7,11 @@ import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -28,13 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
     String password= "1233";
 
+    SharedPreferences pref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //
         binding.category1.setOnClickListener(v-> clickedCategory("커피 메뉴로 이동", 0));
         binding.category2.setOnClickListener(v-> clickedCategory("파르페 메뉴로 이동", 1));
         binding.category3.setOnClickListener(v-> clickedCategory("밀크티 메뉴로 이동", 2));
@@ -42,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
         binding.category5.setOnClickListener(v-> clickedCategory("음료 메뉴로 이동", 4));
 
         binding.settings.setOnClickListener(v-> clickedSettings());
+
+        // 디바이스에 저장된 비밀번호 가져오기
+        pref= getSharedPreferences("login", MODE_PRIVATE);
+        password= pref.getString("password", password);
 
         checkPermission(); // 외부저장소 권한요청
     }
@@ -86,25 +92,70 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog dialog = builder.create();
         dialog.show();
 
-
         // 비밀번호 변경 버튼 누르면 비밀번호 변경 다이얼로그 오픈
         setPw.setOnClickListener(v -> {
             Toast.makeText(this, "비밀번호 변경하기", Toast.LENGTH_SHORT).show();
             dialog.dismiss();
 
+            LinearLayout setPwLayout = (LinearLayout) vi.inflate(R.layout.dialog_set_pw, null);
+
+            EditText etCurrentPw= setPwLayout.findViewById(R.id.current_pw);
+            EditText etNewPw= setPwLayout.findViewById(R.id.new_pw);
+            EditText etNewPwConfirm= setPwLayout.findViewById(R.id.new_pw_confirm);
+
+
+
             AlertDialog.Builder builders= new AlertDialog.Builder(this)
-                    .setView(R.layout.dialog_set_pw)
-                    .setCancelable(true);
+                    .setView(setPwLayout)
+                    .setCancelable(true)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    Log.d("password", "현재비번 : " + etCurrentPw.getText().toString() + ", 새로운비번 : " + etNewPw.getText().toString() + ", 새로운비번확인 : " + etNewPwConfirm.getText().toString());
+
+                    String currentPw= etCurrentPw.getText().toString();
+                    String newPw= etNewPw.getText().toString();
+                    String newPwConfirm= etNewPwConfirm.getText().toString();
+
+
+                    if (newPw.equals(password) || newPwConfirm.equals(password)){
+                        Toast.makeText(MainActivity.this, "이미 설정된 비밀번호입니다.", Toast.LENGTH_SHORT).show();
+
+                    }else if (!newPw.equals(newPwConfirm)){
+                        Toast.makeText(MainActivity.this, "새비밀번호와 새비밀번호 확인이 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+
+                    }else if (!currentPw.equals(password)){
+                        Toast.makeText(MainActivity.this, "현재 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                    else if (newPw.equals(newPwConfirm) && currentPw.equals(password) && !newPw.equals(password) && !newPwConfirm.equals(password)){
+
+                        password= newPw;
+                        pref = getSharedPreferences("login", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = pref.edit();
+
+                        // 디바이스에 변경된 비밀번호 저장
+                        editor.putString("password", password);
+                        editor.commit();
+                        Toast.makeText(MainActivity.this, "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(MainActivity.this, "비밀번호 변경취소", Toast.LENGTH_SHORT).show();
+                }
+            });
 
             AlertDialog dialogs = builders.create();
             dialogs.show();
 
             WindowManager.LayoutParams params=dialogs.getWindow().getAttributes();
             params.width= 700;
-            params.height= 400;
+            params.height= 370;
             dialogs.getWindow().setAttributes(params);
         });
-        setPw.setVisibility(View.INVISIBLE);
+        //setPw.setVisibility(View.INVISIBLE);
     }
 
     // 외부저장소 권한요청
